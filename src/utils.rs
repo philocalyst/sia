85ae0b935ecb9cb8657eac275d98a3fff5c4bbce
unknown <regime_skylark.0y@icloud.com>
2025-05-25 22:30:09 -0400

    🛠️FIXED🛠️ to match the new API for fonts

--- src/utils.rs
+++ src/utils.rs
@@ -1,13 +1,5 @@
 use crate::{Dimensions, FontConfig};
 
-pub(crate) fn get_char_width(c: char, font: &FontConfig) -> f32 {
-    font.font
-        .glyph(c)
-        .scaled(font.scale)
-        .h_metrics()
-        .advance_width
-}
-
 pub fn get_canvas_height(
     pref_dimensions: Option<Dimensions>,
     num_lines: usize,
@@ -17,22 +9,10 @@ pub fn get_canvas_height(
         dims.height as f32
     } else {
         // Get vertical metrics & compute line height
-        let v_metrics = font.font.v_metrics(font.scale);
-        let line_height = (v_metrics.ascent - v_metrics.descent + v_metrics.line_gap) * 1.2;
+        let v_metrics = font.font.metrics('A', font.font_size);
+        let line_height = v_metrics.height;
 
         // Compute total height in px (and add one extra line’s worth of padding)
-        line_height * (num_lines as f32 + 1.0)
+        line_height as f32 * (num_lines as f32 + 1.0)
     }
-}
-
-pub fn get_text_info(str: &str) -> (u32, u32) {
-    let lines: Vec<String> = str.lines().map(|s| s.to_owned()).collect();
-
-    // Figure out the widest line in “characters”
-    let max_chars: u32 = lines.iter().map(|line| line.len()).max().unwrap_or(0) as u32;
-
-    // Total number of lines
-    let line_count: u32 = lines.len() as u32;
-
-    (max_chars, line_count)
 }
