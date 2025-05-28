58a001b89a655de51e99ed0b9ec9cd9aa93d9c4e
unknown <regime_skylark.0y@icloud.com>
2025-05-28 15:08:13 -0400

    🛠️FIXED🛠️ up the formatting and commenting in utils

--- src/utils.rs
+++ src/utils.rs
@@ -1,24 +1,16 @@
-use crate::{Dimensions, FontConfig};
+use crate::FontConfig;
 use rusttype::{self, Scale};
 use std::fs;
 
-pub fn get_canvas_height(
-    pref_dimensions: Option<Dimensions>,
-    num_lines: usize,
-    font: &FontConfig,
-) -> f32 {
-    if let Some(dims) = pref_dimensions {
-        dims.height as f32
-    } else {
-        let bytes = fs::read(font.font_path.clone()).unwrap();
-        let font_font = rusttype::Font::try_from_bytes(&bytes).unwrap();
-        // Get vertical metrics & compute line height
+pub fn get_canvas_height(num_lines: usize, font: &FontConfig) -> f32 {
+    // Read into RUSTTYPE as fontdue sucks at height
+    let bytes = fs::read(font.font_path.clone()).unwrap();
+    let font_font = rusttype::Font::try_from_bytes(&bytes).unwrap();
 
-        let scale = Scale::uniform(font.font_size);
-        let v_metrics = font_font.v_metrics(scale);
-        let line_height = (v_metrics.ascent - v_metrics.descent + v_metrics.line_gap) * 1.2;
+    // Get vertical metrics & find individual line height
+    let scale = Scale::uniform(font.font_size);
+    let v_metrics = font_font.v_metrics(scale);
+    let line_height = (v_metrics.ascent - v_metrics.descent + v_metrics.line_gap) * 1.2;
 
-        // Compute total height in px (and add one extra line’s worth of padding)
-        line_height * num_lines as f32
-    }
+    line_height * num_lines as f32
 }
